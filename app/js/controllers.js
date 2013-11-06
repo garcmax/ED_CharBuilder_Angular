6796c2e287870438494459aafa7a94eef37dba6f
Maximilien Garcia <niwamaxou@gmail.com>
2013-11-06 11:25:52 +0100

    Discipline selecter

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,17 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
+  controller('MyCtrl1', function($scope, $http) {
+	  	$http.get('resources/disciplines/disciplines.json').success(function(data) {
+    	$scope.disc = data;
+  	});	
+  })
   .controller('MyCtrl2', [function() {
 
   }]);
+  
+  function DisciplinesController($scope, $http) {
+  	$http.get('resources/disciplines/disciplines.json').success(function(data) {
+    	$scope.disciplines = data;
+  	});
+  }
